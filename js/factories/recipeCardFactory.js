eeaa068cfb66104d05a22554104c4703a15ae496
Vinodh Zamboulingame <vzamboulingame@gmail.com>
2023-01-31 16:16:51 +0100

    Added ingredients dom to recipeCardFactory

--- js/factories/recipeCardFactory.js
+++ js/factories/recipeCardFactory.js
@@ -1,48 +1,66 @@
 export function recipeCardFactory(data) {
   // Destructuring the data object to extract its properties
-  const { id, name, ingredients, time, description } = data;
+  const { name, ingredients, time, description } = data;
+
+  // Function that will generate the recipe card header innerHTML
+  function getRecipeCardHeaderDOM() {
+    const recipeCardHeaderDOM = `
+    <div class="recipes-card-header">
+        <p class="recipes-card-title">${name}</p>
+        <div class="recipes-card-duration">
+            <i class="fa-regular fa-clock"></i>
+            <p>${time} min</p>
+        </div>
+    </div>
+    `;
+    return recipeCardHeaderDOM;
+  }
+
+  // Function that will generate the recipe card desc innerHTML
+  function getRecipeCardDescDOM() {
+    let recipeCardIngredients = "";
+
+    ingredients.forEach((element) => {
+      const recipeName = element.ingredient;
+      const recipeQty = element.quantity ? element.quantity : "";
+      const recipeUnit = element.unit ? element.unit : "";
+
+      recipeCardIngredients += `
+        <li class="recipes-card-ingredients-item">
+            <p class="recipes-card-ingredients-name">${recipeName}:</p>
+            <p class="recipes-card-ingredients-qty">${recipeQty} ${recipeUnit}</p>
+        </li>
+        `;
+    });
+
+    const recipeCardDescDOM = `
+    <div class="recipes-card-desc">
+        <ul class="recipes-card-ingredients">
+        ${recipeCardIngredients}
+        </ul>
+        <p class="recipes-card-todo">${description}</p>
+    </div>
+    `;
+
+    return recipeCardDescDOM;
+  }
 
   // Function that will return a DOM element for the recipe card
   function getRecipeCardDOM() {
-    // Create a list item element to contain the media card
     const listitem = document.createElement("li");
     listitem.className += "recipes-card";
-    listitem.id = id;
 
-    // Generate the innerHTML of the list item element to contain the recipe card
-    listitem.innerHTML = `
-    <div class="recipes-card-img"></div>
-    <div class="recipes-card-info">
-      <div class="recipes-card-header">
-        <p class="recipes-card-title">${name}</p>
-        <div class="recipes-card-duration">
-          <i class="fa-regular fa-clock"></i>
-          <p>${time} min</p>
-        </div>
-      </div>
-      <div class="recipes-card-desc">
-        <ul class="recipes-card-ingredients">
-          <li class="recipes-card-ingredients-item">
-            <p class="recipes-card-ingredients-name">Ingrédient:</p>
-            <p class="recipes-card-ingredients-qty">100g</p>
-          </li>
-          <li class="recipes-card-ingredients-item">
-            <p class="recipes-card-ingredients-name">Ingrédient:</p>
-            <p class="recipes-card-ingredients-qty">100g</p>
-          </li>
-          <li class="recipes-card-ingredients-item">
-            <p class="recipes-card-ingredients-name">Ingrédient:</p>
-            <p class="recipes-card-ingredients-qty">100g</p>
-          </li>
-          <li class="recipes-card-ingredients-item">
-            <p class="recipes-card-ingredients-name">Ingrédient:</p>
-            <p class="recipes-card-ingredients-qty">100g</p>
-          </li>
-        </ul>
-        <p class="recipes-card-todo">${description}</p>
-      </div>
-    </div>
-      `;
+    const recipesCardImg = document.createElement("div");
+    recipesCardImg.className += "recipes-card-img";
+
+    const recipesCardInfo = document.createElement("div");
+    recipesCardInfo.className += "recipes-card-info";
+
+    recipesCardInfo.innerHTML += getRecipeCardHeaderDOM();
+    recipesCardInfo.innerHTML += getRecipeCardDescDOM();
+
+    listitem.append(recipesCardImg);
+    listitem.append(recipesCardInfo);
 
     // Return the listitem element
     return listitem;
