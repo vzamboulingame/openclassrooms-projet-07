6ce4fbb2af459ee0fe6624e9fbf29e7f23926245
Vinodh Zamboulingame <vzamboulingame@gmail.com>
2023-02-01 12:09:42 +0100

    Created const variables for :
    - unique and sorted ingredients array
    - unique and sorted appliances array
    - unique and sorted ustensils array

--- js/main.js
+++ js/main.js
@@ -3,6 +3,22 @@ import { recipeCardFactory } from "./factories/recipeCardFactory.js";
 
 let filteredRecipesArray = recipesArray;
 
+const uniqueIngredientsArray = Array.from(
+  new Set(
+    recipesArray.flatMap((recipe) =>
+      recipe.ingredients.map((ingredient) => ingredient.ingredient)
+    )
+  )
+).sort();
+
+const uniqueAppliancesArray = Array.from(
+  new Set(recipesArray.map((recipe) => recipe.appliance))
+).sort();
+
+const uniqueUstensilsArray = Array.from(
+  new Set(recipesArray.flatMap((recipe) => recipe.ustensils))
+).sort();
+
 // Function to render recipe cards list
 function renderRecipeCards(array) {
   const recipeCardsList = document.querySelector(".recipes");
