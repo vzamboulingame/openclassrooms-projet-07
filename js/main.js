203ef849314b56c36d16bc4da9428e48dede6ff8
Vinodh Zamboulingame <vzamboulingame@gmail.com>
2023-02-14 17:17:55 +0100

    Added jsdoc comments to main.js

--- js/main.js
+++ js/main.js
@@ -34,7 +34,12 @@ let filterTagArray = [];
 
 /* FUNCTIONS */
 
-// Function to render recipe cards list
+/**
+ * Takes an array of recipe objects as parameter, gets the DOM element
+ * for each recipe object and appends it to the recipe cards list
+ *
+ * @param {array} array - An array of recipe objects
+ */
 function renderRecipeCards(array) {
   const recipeCardsList = document.querySelector(".recipes");
 
@@ -47,7 +52,13 @@ function renderRecipeCards(array) {
   });
 }
 
-// Function to render filter list items
+/**
+ * Takes an id and an array as parameters, gets the DOM element
+ * for each filter list item and appends it to the filter list element
+ *
+ * @param {string} id - The id of the filter list element
+ * @param {array} array - An array of filter list items
+ */
 async function renderFilterListItems(id, array) {
   const filterList = document.getElementById(id);
 
@@ -61,7 +72,14 @@ async function renderFilterListItems(id, array) {
   });
 }
 
-// Function to render filter tag spans
+/**
+ * Takes a tag and color as parameters, then renders the DOM element for the form tag span element,
+ * appends it to the form tag container and adds an event listener to the tag span element's
+ * close button that removes the tag from the filter tag array and re-renders the recipe elements
+ *
+ * @param {string} tag - The tag that will be rendered
+ * @param {string} color - The color of the tag
+ */
 function renderFormTagSpan(tag, color) {
   const formTagContainer = document.querySelector(".form-tag-container");
 
@@ -84,7 +102,11 @@ function renderFormTagSpan(tag, color) {
   });
 }
 
-// Function to display filter dropdown
+/**
+ * Takes a filter container element as parameter and displays its filter list dropdown
+ *
+ * @param {object} element - The filter container element object
+ */
 function displayFormFilterDropdown(element) {
   const formFilterContainer = element;
   const formFilterList = formFilterContainer.querySelector(".form-filter-list");
@@ -92,13 +114,19 @@ function displayFormFilterDropdown(element) {
     ".form-filter-chevron"
   );
 
+  console.log(typeof element);
+
   formFilterContainer.classList.add("form-filter-dropdown");
   formFilterChevron.classList.replace("fa-chevron-down", "fa-chevron-up");
   formFilterList.style.display = "grid";
   formFilterList.setAttribute("aria-hidden", "false");
 }
 
-// Function to close filter dropdown
+/**
+ * Takes a filter container element as parameter and closes its filter list dropdown
+ *
+ * @param {object} element - The filter container element object
+ */
 function closeFormFilterDropdown(element) {
   const formFilterContainer = element;
   const formFilterList = formFilterContainer.querySelector(".form-filter-list");
@@ -112,7 +140,10 @@ function closeFormFilterDropdown(element) {
   formFilterList.setAttribute("aria-hidden", "true");
 }
 
-// Function to refresh all filter arrays data
+/**
+ * Refreshes the ingredientsArray, appliancesArray, ustensilsArray arrays from the
+ * filtered recipes array and updates the listArrayMapping array of objects
+ */
 async function refreshFilterArrays() {
   ingredientsArray = await getIngredientsArray(filteredRecipesArray);
   appliancesArray = await getAppliancesArray(filteredRecipesArray);
@@ -125,7 +156,10 @@ async function refreshFilterArrays() {
   ];
 }
 
-// Function to render all elements
+/**
+ * Refresh all list item arrays from the filtered recipes array and
+ * re-renders the recipe cards and all the filter list tems
+ */
 async function renderAllElements() {
   await refreshFilterArrays();
 
@@ -139,7 +173,6 @@ async function renderAllElements() {
 /* EVENT LISTENERS */
 
 // Event listeners for the main search input
-
 formSearchInput.addEventListener("input", () => {
   if (formSearchInput.value.length >= 3) {
     console.log(formSearchInput.value);
@@ -147,7 +180,6 @@ formSearchInput.addEventListener("input", () => {
 });
 
 // Event listeners for each form filter container child elements
-
 formFilterContainers.forEach((element) => {
   const formFilterContainer = element;
   const formFilterList = formFilterContainer.querySelector(".form-filter-list");
