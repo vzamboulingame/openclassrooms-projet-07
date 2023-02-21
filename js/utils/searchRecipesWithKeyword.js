07509762dba9abf1809635e7d868eda8ff9386be
Vinodh Zamboulingame <vzamboulingame@gmail.com>
2023-02-21 09:38:37 +0100

    Added empty searchRecipesWithKeyword function

---
+++ js/utils/searchRecipesWithKeyword.js
@@ -0,0 +1,9 @@
+/**
+ * Takes a keyword and a recipes array as parameters, then filters the recipes array
+ * based on the keyword, and returns a filtered array of matching recipes
+ *
+ * @param {string} keyword - The keyword that the user is searching for
+ * @param {array} array - The array of recipes to search through
+ * @returns {array} - An array of recipes that match the keyword.
+ */
+export function searchRecipesWithKeyword(keyword, array) {}
