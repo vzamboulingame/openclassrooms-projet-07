46d6728655e5f794ea19e2258d4aba8c7aaa436f
Vinodh Zamboulingame <vzamboulingame@gmail.com>
2023-02-21 10:22:55 +0100

    Added for loop version of searchRecipesWithKeyword

--- js/utils/searchRecipesWithKeyword.js
+++ js/utils/searchRecipesWithKeyword.js
@@ -6,4 +6,34 @@
  * @param {array} array - The array of recipes to search through
  * @returns {array} - An array of recipes that match the keyword.
  */
-export function searchRecipesWithKeyword(keyword, array) {}
+export function searchRecipesWithKeyword(keyword, array) {
+  const filteredRecipes = [];
+
+  for (let i = 0; i < array.length; i++) {
+    const recipe = array[i];
+
+    const nameWithKeyword = recipe.name
+      .toLowerCase()
+      .includes(keyword.toLowerCase());
+    const descriptionWithKeyword = recipe.description
+      .toLowerCase()
+      .includes(keyword.toLowerCase());
+
+    let ingredientsWithKeyword = false;
+
+    for (let n = 0; n < recipe.ingredients.length; n++) {
+      const ingredient = recipe.ingredients[n];
+
+      if (ingredient.ingredient.toLowerCase().includes(keyword.toLowerCase())) {
+        ingredientsWithKeyword = true;
+        break;
+      }
+    }
+
+    if (nameWithKeyword || descriptionWithKeyword || ingredientsWithKeyword) {
+      filteredRecipes.push(recipe);
+    }
+  }
+
+  return filteredRecipes;
+}
